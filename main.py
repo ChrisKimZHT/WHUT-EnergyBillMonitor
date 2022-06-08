7b6ee220addb58e7153b53412ed1c4313a38288b
ChrisKim <1515748548@qq.com>
2022-06-08 22:58:13 +0800

    新增电量不足提醒

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ def main():
            f"使用电量: {round(float(data['roomlist']['ZVlaue']) - float(history), 2)} kW·h（距上次运行）\n" \
            f"查表时间: {data['roomlist']['readTime']}"
     save_history(data['roomlist']['ZVlaue'])
+    if float(data['roomlist']['remainPower']) < 15.00:
+        text += "\n[!] 电量不足，注意及时充值 [!]"
     print(text)
     send_msg(text)
 
