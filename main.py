04ba9385e5bec49f7385c99502d6c3914aa0bfca
ChrisKim <1515748548@qq.com>
2022-07-15 23:09:55 +0800

    添加版本号和作者

--- main.py
+++ main.py
@@ -9,12 +9,13 @@ if __name__ == "__main__":
     data = query_data()
     log.debug(f"取得数据: {data}")
     if data["status"]:
-        text = f"【用电情况】\n" \
+        text = f"【用电情况】v1.0.0\n" \
                f"剩余电量: {data['remain']} kW·h\n" \
                f"累计电量: {data['total']} kW·h\n" \
                f"使用电量: {round(data['total'] - get_history(), 2)} kW·h（距上次运行）"
         if not conf_dormitory["is_mafangshan"]:
             text += f"\n查表时间: {data['time']}"
+        text += "\nGitHub@ChrisKim"
         if data['remain'] < 15.00:
             text += "\n[!] 电量不足，注意及时充值 [!]"
         log.debug(f"生成消息完成: \n{text}")
